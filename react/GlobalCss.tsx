4b54df20b7466828bf1799db5eaef8ad1abd3041
Andre Gonçalves <dregoncalves22@gmail.com>
2025-07-30 10:43:03 -0300

    update

---
+++ react/GlobalCss.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./src/styles/global.css";
+
+const GlobalCss: StorefrontFunctionComponent = () => {
+    return <span className={styles.global}/>
+};
+
+export default GlobalCss;
